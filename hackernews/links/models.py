4eb5241e8b7deb3d05d3d8041f23de880090efbd
helloitsjoe <jboyle617@gmail.com>
2020-04-09 07:35:02 -0400

    Add votes

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -7,3 +7,7 @@ class Link(Model):
     url = URLField()
     description = TextField(blank=True)
     posted_by = ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=CASCADE)
+
+class Vote(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    link = ForeignKey('links.Link', related_name='votes', on_delete=CASCADE)
--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -1,30 +1,37 @@
-import graphene
+from graphene import ObjectType, List, Int, String, Field, Mutation
 from graphene_django import DjangoObjectType
 
-from .models import Link
+from .models import Link, Vote
 from users.schema import UserType
 
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
-class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+
+class Query(ObjectType):
+    links = List(LinkType)
+    votes = List(VoteType)
     
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
 
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
-class CreateLink(graphene.Mutation):
-    id = graphene.Int()
-    url = graphene.String()
-    description = graphene.String()
-    posted_by = graphene.Field(UserType)
+class CreateLink(Mutation):
+    id = Int()
+    url = String()
+    description = String()
+    posted_by = Field(UserType)
 
     class Arguments:
-        url = graphene.String()
-        description = graphene.String()
+        url = String()
+        description = String()
 
     def mutate(self, info, url, description):
         user = info.context.user or None
@@ -43,6 +50,30 @@ class CreateLink(graphene.Mutation):
             posted_by=link.posted_by,
         )
 
+class CreateVote(Mutation):
+    user = Field(UserType)
+    link = Field(LinkType)
 
-class Mutation(graphene.ObjectType):
+    class Arguments:
+        link_id = Int()
+
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('You must be logged in to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+
+
+class Mutation(ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
