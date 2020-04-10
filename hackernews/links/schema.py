5277e76d680b177e12a45b772b601338fd831159
helloitsjoe <jboyle617@gmail.com>
2020-04-10 05:53:45 -0400

    Filtering

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -1,5 +1,7 @@
 from graphene import ObjectType, List, Int, String, Field, Mutation
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
+from django.db.models import Q
 
 from .models import Link, Vote
 from users.schema import UserType
@@ -14,10 +16,17 @@ class VoteType(DjangoObjectType):
 
 
 class Query(ObjectType):
-    links = List(LinkType)
+    links = List(LinkType, search=String())
     votes = List(VoteType)
-    
-    def resolve_links(self, info, **kwargs):
+
+    def resolve_links(self, info, search=None, **kwargs):
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+
         return Link.objects.all()
 
     def resolve_votes(self, info, **kwargs):
@@ -60,7 +69,7 @@ class CreateVote(Mutation):
     def mutate(self, info, link_id):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('You must be logged in to vote!')
+            raise GraphQLError('You must be logged in to vote!')
 
         link = Link.objects.filter(id=link_id).first()
         if not link:
