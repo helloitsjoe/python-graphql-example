59602e75f6aece32233273507eacc6d8c3421743
helloitsjoe <jboyle617@gmail.com>
2020-05-15 06:34:47 -0400

    cleanup

--- hackernews/users/schema.py
+++ hackernews/users/schema.py
@@ -1,15 +1,17 @@
 from django.contrib.auth import get_user_model
 
-from graphene import ObjectType, AbstractType, Mutation, List, Field, String
+import graphene
 from graphene_django import DjangoObjectType
+
 
 class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
 
-class Query(AbstractType):
-    me = Field(UserType)
-    users = List(UserType)
+
+class Query(graphene.AbstractType):
+    me = graphene.Field(UserType)
+    users = graphene.List(UserType)
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
@@ -17,27 +19,26 @@ class Query(AbstractType):
     def resolve_me(self, info):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Not logged in!')
+            raise Exception("Not logged in!")
 
         return user
 
-class CreateUser(Mutation):
-    user = Field(UserType)
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
 
     class Arguments:
-        username = String(required=True)
-        password = String(required=True)
-        email = String(required=True)
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
 
     def mutate(self, info, username, password, email):
-        user = get_user_model()(
-            username=username,
-            email=email,
-        )
+        user = get_user_model()(username=username, email=email,)
         user.set_password(password)
         user.save()
 
         return CreateUser(user=user)
 
-class Mutation(ObjectType):
+
+class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
